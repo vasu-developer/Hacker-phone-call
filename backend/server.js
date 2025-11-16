4ba50eaeb902c6220ae39c3e0dd862ee3668e14d
Vasudev Verma <123830315+verma952@users.noreply.github.com>
2025-11-16 21:46:55 +0530

    another update

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,22 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+// CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 const {
